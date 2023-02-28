becd2210d4aad473e789816ec1c5a4601cc74dbc
Angeldog27 <126614235+Angeldog27@users.noreply.github.com>
2023-02-28 18:13:20 -0500

    Created HTML,CSS & JS files

---
+++ script.js
@@ -0,0 +1,49 @@
+let now = new Date();
+let h5 = document.querySelector("h5");
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+h5.innerHTML = `${day},${hours}:${minutes}`;
+
+function handleSumbit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+  let h4 = document.querySelector("h4");
+  h4.innerHTML = `${city}`;
+}
+function searchCity(city) {
+  let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function showTemperature(response) {
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSumbit);
