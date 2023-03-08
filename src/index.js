3f71fd825c228174a414254790b2ddb70926159a
Angeldog27 <126614235+Angeldog27@users.noreply.github.com>
2023-03-07 20:38:26 -0500

    Updated img src  Alt

--- src/index.js
+++ src/index.js
@@ -43,10 +43,11 @@ function showTemperature(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-  iconElement.setAttribute(
+  icon.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  icon.setAttribute("alt", response.data.weather[0].main);
 }
 
 let form = document.querySelector("#search-form");
